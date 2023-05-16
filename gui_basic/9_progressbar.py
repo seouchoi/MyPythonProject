89120a5600fe79866b108e2f75612e181bf31af8
서우 최 <tjdn1080@gmail.com>
2023-05-16 16:39:32 +0900

    2023/05/16

--- gui_basic/9_progressbar.py
+++ gui_basic/9_progressbar.py
@@ -24,7 +24,7 @@ progressbar2 = ttk.Progressbar(root, maximum=100,length=150, variable=p_var2)
 progressbar2.pack()
 
 def btncmd2():
-    for i in range(1,100): #1~100까지
+    for i in range(1,101): #1~100까지
         time.sleep(0.01) #0.01초 대기
 
         p_var2.set(i) #progress bar의 값 설정
